22a88cb9c9970d92a366524ec4853e6176296452
XionIhsaka <XionIhsaka@pm.me>
2025-09-29 17:35:09 +0200

    First upload

---
+++ main.py
@@ -0,0 +1,27 @@
+import pygame
+from constants import *
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+# Game loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+               return
+
+# Black screen
+    screen.fill((0, 0, 0))
+
+    pygame.display.flip()
+
+
+    print("Starting Asteroids!")
+
+if __name__ == "__main__":
+    main()
+
+print(f"Screen width: {SCREEN_WIDTH}")
+print(f"Screen height: {SCREEN_HEIGHT}")
